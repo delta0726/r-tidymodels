df060d6808c7ff51c305e303871b1b0c83ddeece
Keido Sumi <delta0726@nta.pial.jp>
2021-02-12 07:56:47 +0900

    Update usage of themis

--- library/themis/basic/basic_usage.R
+++ library/themis/basic/basic_usage.R
@@ -15,9 +15,15 @@
 # - https://qiita.com/eigs/items/8ae0970afe188a1124d1
 
 
+# ＜目次＞
+# 0 準備
+# 1 オーバーサンプリングの実行
+# 2 オーバーサンプリングの結果検証
+# 3 オーバーサンプリングのサンプル比率変更
+# 4 アンダーサンプリングのサンプル比率変更
 
 
-# 1 準備 -----------------------------------------------------
+# 0 準備 ---------------------------------------------------------------
 
 # ライブラリ準備
 library(tidyverse)
@@ -25,20 +31,36 @@ library(recipes)
 library(modeldata)
 library(themis)
 library(gridExtra)
+library(magrittr)
 
 
 # データ準備
+# --- stem教育を受けた人のデータ
 data(okc)
-
 
 # データ確認
 okc %>% as_tibble()
 okc %>% glimpse()
 
 
+# データ準備
+# ---- カテゴリごとのサンプルデータ
+example_data <-
+  data.frame(class = letters[rep(1:5, 1:5 * 10)],
+             x = rnorm(150))
+
+# データ確認
+example_data %>% as_tibble()
+example_data %>% pull(class) %>% table()
+
+# プロット
+# --- カテゴリaからeにかけて順に多くなる
+example_data %>%
+  ggplot(aes(class)) +
+  geom_bar()
 
 
-# 2-1 簡単な例 -----------------------------------------------------
+# 1 オーバーサンプリングの実行 -------------------------------------------
 
 # ＜ポイント＞
 # - SMOTEアルゴリズムによるオーバーサンプリングを行う
@@ -46,9 +68,8 @@ okc %>% glimpse()
 
 
 # ラベルデータの数
-# --- 不均衡になっている
+# --- stemとotherが不均衡になっている
 okc$Class %>% table(useNA = "always") %>% sort()
-
 
 # レシピ作成
 # --- 欠損値を平均で補完
@@ -59,31 +80,47 @@ ds_rec <-
     step_smote(Class) %>%
     prep()
 
-
 # ラベルデータの数
-# --- 前処理後は均衡になっている
-# ---
-ds_rec %>% juice() %>% pull(Class) %>% table(useNA = "always") %>% sort()
+# --- stepが増加してotherと同じサンプル数になっている
+ds_rec %>%
+  juice() %>%
+  pull(Class) %>%
+  table(useNA = "always") %>%
+  sort()
 
 
-
-# 2-2 処理の検証 -----------------------------------------------
+# 2 オーバーサンプリングの結果検証 ----------------------------------------
 
 # データ取得
 df_raw <- okc %>% select(Class, age, height)
 df_rec <- ds_rec %>% juice() %>% select(Class, age, height)
 
+# レコード数の確認
+df_raw %>% dim()
+df_rec %>% dim()
 
-# 確認
-df_raw %>% print()
-df_rec %>% print()
-
+# 同一レコード数
+# --- 実験：同じレコードを増やしているだけならグループ数はレシピ前後で同じになるはず
+# --- SMOTEは同じレコードを単純に増えているわけではない
+df_raw %>% group_by(Class, age, height) %>% tally() %>% use_series(n) %>% sum()
+df_rec %>% group_by(Class, age, height) %>% tally() %>% use_series(n) %>% sum()
 
 # 統計量
 # --- データのレンジは一致
 df_raw %>% summary()
 df_rec %>% summary()
 
+# ヒストグラム
+# --- age
+p1 <- df_raw %>% ggplot(aes(x = age)) + geom_histogram(alpha=0.5) + ggtitle("raw")
+p2 <- df_rec %>% ggplot(aes(x = age)) + geom_histogram(alpha=0.5) + ggtitle("rec")
+grid.arrange(p1, p2, nrow = 1)
+
+# ヒストグラム
+# --- hight
+p1 <- df_raw %>% ggplot(aes(x = height)) + geom_histogram(alpha=0.5) + ggtitle("raw")
+p2 <- df_rec %>% ggplot(aes(x = height)) + geom_histogram(alpha=0.5) + ggtitle("rec")
+grid.arrange(p1, p2, nrow = 1)
 
 # 散布図
 p1 <- df_raw %>% ggplot(aes(x = age, y = height, fill = Class, color = Class)) + geom_point(alpha=0.5)
@@ -91,27 +128,7 @@ p2 <- df_rec %>% ggplot(aes(x = age, y = height, fill = Class, color = Class)) +
 grid.arrange(p1, p2)
 
 
-
-
-# 3-1 データ準備 -----------------------------------------------------
-
-# データ準備
-example_data <- data.frame(class = letters[rep(1:5, 1:5 * 10)],
-                           x = rnorm(150))
-
-
-# データ確認
-example_data %>% as_tibble()
-example_data %>% pull(class) %>% table()
-
-
-# プロット
-example_data %>%
-  ggplot(aes(class)) +
-  geom_bar()
-
-
-# 3.2 step_upsample() ------------------------------------------
+# 3 オーバーサンプリングのサンプル比率変更 -------------------------------------------
 
 # ＜ポイント＞
 # - オーバーサンプリングの各関数にはover_ratio引数が設定されている
@@ -119,20 +136,23 @@ example_data %>%
 #   --- 0の場合は処理なし、1の場合は完全に均衡となる
 
 
-# 3.2.1 over_ratio = 1 ----------------------------------------
+# カテゴリごとのカウント
+# --- 元データ
+example_data %>% pull(class) %>% table()
+
+
+# over_ratio = 1 ------------------------------------------------------
 
 # 前処理
-# --- オーバーサンプリング
+# --- 全て最大数と同じになるようにアップサンプリング
 df_up_1 <-
-recipe(~., example_data) %>%
-  step_upsample(class, over_ratio = 1) %>%
-  prep() %>%
-  juice()
-
+  recipe(~., example_data) %>%
+    step_upsample(class, over_ratio = 1) %>%
+    prep() %>%
+    juice()
 
 # データ確認
 df_up_1 %>% pull(class) %>% table()
-
 
 # プロット
 # --- サンプル数
@@ -140,9 +160,9 @@ df_up_1 %>%
   ggplot(aes(class)) +
   geom_bar()
 
-
-# プロット
-# --- データレンジ
+# プロット作成
+# --- 元データの分布を元にアップサンプリング
+# --- レンジに変化なし
 df1 <- example_data %>% mutate(flag = "raw")
 df2 <- df_up_1 %>% mutate(flag = "reciped")
 df1 %>%
@@ -153,20 +173,18 @@ df1 %>%
 
 
 
-# 3.2.2 over_ratio = 0.5 ----------------------------------------
+# 3 over_ratio = 0.5 -----------------------------------------------------
 
 # 前処理
-# --- オーバーサンプリング
+# --- 最大数の半分まで増やすようにアップサンプリング
 df_up_0.5 <-
 recipe(~., example_data) %>%
   step_upsample(class, over_ratio = 0.5) %>%
   prep() %>%
   juice()
 
-
 # データ確認
 df_up_0.5 %>% pull(class) %>% table()
-
 
 # プロット
 # --- サンプル数
@@ -174,9 +192,9 @@ df_up_0.5 %>%
   ggplot(aes(class)) +
   geom_bar()
 
-
-# プロット
-# --- データレンジ
+# プロット作成
+# --- 元データの分布を元にアップサンプリング
+# --- レンジに変化なし
 df1 <- example_data %>% mutate(flag = "raw")
 df2 <- df_up_0.5 %>% mutate(flag = "reciped")
 df1 %>%
@@ -186,27 +204,24 @@ df1 %>%
   facet_wrap(~flag)
 
 
-
-# 3.3 step_downsample() ------------------------------------------
+# 4 アンダーサンプリングのサンプル比率変更 ----------------------------------------
 
 # ＜ポイント＞
 # - アンダーサンプリングの各関数には_ratio引数が設定されている
 
 
-# 3.2.1 over_ratio = 1 ----------------------------------------
+# 3.2.1 over_ratio = 1 ------------------------------------------------
 
 # 前処理
-# --- オーバーサンプリング
+# --- 最小値に一致するようにダウンサンプリング
 df_down_1 <-
 recipe(~., example_data) %>%
   step_downsample(class, under_ratio = 1) %>%
   prep() %>%
   juice()
 
-
 # データ確認
 df_down_1 %>% pull(class) %>% table()
-
 
 # プロット
 # --- サンプル数
@@ -214,9 +229,9 @@ df_down_1 %>%
   ggplot(aes(class)) +
   geom_bar()
 
-
-# プロット
-# --- データレンジ
+# プロット作成
+# --- 元データの分布を元にダウンサンプリング
+# --- レンジに変化なし
 df1 <- example_data %>% mutate(flag = "raw")
 df2 <- df_down_1 %>% mutate(flag = "reciped")
 df1 %>%
@@ -226,27 +241,24 @@ df1 %>%
   facet_wrap(~flag)
 
 
-# 3.2.2 over_ratio = 0.5 ----------------------------------------
+# over_ratio = 0.5 ------------------------------------------------
 
 # 前処理
-# --- オーバーサンプリング
+# --- 最小値の2倍まで減らすようにダウンサンプリング
 df_down_2 <-
 recipe(~., example_data) %>%
   step_downsample(class, under_ratio = 2) %>%
   prep() %>%
   juice()
 
-
 # データ確認
 df_down_2 %>% pull(class) %>% table()
-
 
 # プロット
 # --- サンプル数
 df_down_2 %>%
   ggplot(aes(class)) +
   geom_bar()
-
 
 # プロット
 # --- データレンジ
